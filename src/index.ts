553ce26a3da531281cef8b1a507c7cf71dd88ff6
Ahurein Ebenezer <ahureinebenezer@gmail.com>
2023-05-23 18:10:30 +0000

    add rate limit

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,24 @@ import express, {Application, Request, Response} from "express";
 import mongoose from "mongoose";
 import 'dotenv/config'
 import { attractionRoutes } from "./routes/attractions.route";
+import rateLimit from 'express-rate-limit'
 
 const app: Application = express()
+
+//configuration
+const limiter = rateLimit({
+    windowMs: 7200000,
+    max: 2,
+    standardHeaders: true,
+    legacyHeaders: false,
+    message: "You have exceeded your request limit, Please wait for a while"
+})
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 app.use(express.text())
+
+app.use(limiter)
 
 const router  = express.Router()
 // register routes
